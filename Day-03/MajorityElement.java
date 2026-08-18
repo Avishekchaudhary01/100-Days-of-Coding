15bd5190d1251c4126da77f8814c28267a4a248e
Avishek Chaudhaary <Avishek.24SCSE1011345@galgotiasuniversity.ac.in>
2026-08-18 23:40:27 +0530

    Day 03: Add Majority Element solution

---
+++ Day-03/MajorityElement.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class MajorityElement {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> frequency = new HashMap<>();
+
+        for (int num : nums) {
+            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
+            if (frequency.get(num) > nums.length / 2) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+}
