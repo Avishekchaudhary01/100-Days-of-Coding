88d8e4ad365a4f2506f43f09cdc864b2aa73248e
Avishek Chaudhaary <Avishek.24SCSE1011345@galgotiasuniversity.ac.in>
2026-08-18 23:40:42 +0530

    Day 03: Add Missing Number solution

---
+++ Day-03/MissingNumber.java
@@ -0,0 +1,12 @@
+class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int xor = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= i;
+            xor ^= nums[i];
+        }
+
+        return xor;
+    }
+}
