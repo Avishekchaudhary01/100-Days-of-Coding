03efa1113409310e765c9bead6e80b65301836dc
Avishek Chaudhaary <Avishek.24SCSE1011345@galgotiasuniversity.ac.in>
2026-08-22 15:13:20 +0530

    Day 07: Add First Bad Version solution

---
+++ Day-07/FirstBadVersion.java
@@ -0,0 +1,19 @@
+// LeetCode provides isBadVersion(int version) in the VersionControl API.
+class FirstBadVersion extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
